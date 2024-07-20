a18d9a9a51fb87f2fbcae9a5c9c3d062adcd862f
Sherif Fouad <130251496+Sheriff30@users.noreply.github.com>
2024-07-21 00:38:26 +0300

    updates

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Animations
 gsap.registerPlugin(ScrollTrigger);
 
 const imageContainers = document.querySelectorAll(".images");
@@ -22,6 +23,7 @@ imageContainers.forEach((container) => {
   );
 });
 
+// MODAL
 document.addEventListener("DOMContentLoaded", () => {
   const modal = document.getElementById("image-modal");
   const modalImg = document.getElementById("modal-image");
@@ -45,4 +47,51 @@ document.addEventListener("DOMContentLoaded", () => {
       modal.style.display = "none";
     }
   };
+});
+
+// Modal navigation logic
+
+document.addEventListener("DOMContentLoaded", () => {
+  const modal = document.getElementById("image-modal");
+  const modalImg = document.getElementById("modal-image");
+  const closeBtn = document.getElementsByClassName("close")[0];
+  const prevBtn = document.querySelector(".prev");
+  const nextBtn = document.querySelector(".next");
+  let currentImgIndex;
+  const images = document.querySelectorAll(".img-contanier img");
+
+  function showImage(index) {
+    if (index < 0) {
+      index = images.length - 1;
+    } else if (index >= images.length) {
+      index = 0;
+    }
+    modal.style.display = "block";
+    modalImg.src = images[index].src;
+    currentImgIndex = index;
+  }
+
+  images.forEach((img, index) => {
+    img.parentElement.addEventListener("click", () => {
+      showImage(index);
+    });
+  });
+
+  closeBtn.onclick = function () {
+    modal.style.display = "none";
+  };
+
+  window.onclick = function (event) {
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  };
+
+  prevBtn.addEventListener("click", () => {
+    showImage(currentImgIndex - 1);
+  });
+
+  nextBtn.addEventListener("click", () => {
+    showImage(currentImgIndex + 1);
+  });
 });
