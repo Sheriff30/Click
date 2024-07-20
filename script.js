9af6bf47832732d9308e5560c79e2ed862f68686
Sherif Fouad <130251496+Sheriff30@users.noreply.github.com>
2024-07-20 11:49:54 +0300

    updates

--- script.js
+++ script.js
@@ -2,23 +2,47 @@ gsap.registerPlugin(ScrollTrigger);
 
 const imageContainers = document.querySelectorAll(".images");
 
-// Loop through each .images container and set up ScrollTrigger animations
 imageContainers.forEach((container) => {
   gsap.fromTo(
     container.querySelectorAll(".img-contanier"),
     {
-      opacity: 0, // Start from opacity 0
+      opacity: 0,
     },
     {
-      opacity: 1, // End at opacity 1
-      y: 0, // Optional: end with no vertical offset
-      duration: 1, // Duration of the animation
-      stagger: 0.2, // Stagger animation for each .img-contanier
+      opacity: 1,
+      y: 0,
+      duration: 1,
+      stagger: 0.2,
       scrollTrigger: {
-        trigger: container, // Element that triggers the animation
-        start: "top bottom", // When the top of the container hits the bottom of the viewport
-        end: "bottom top", // When the bottom of the container hits the top of the viewport
+        trigger: container,
+        start: "top bottom",
+        end: "bottom top",
       },
     }
   );
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const modal = document.getElementById("image-modal");
+  const modalImg = document.getElementById("modal-image");
+  const captionText = document.getElementById("caption");
+  const closeBtn = document.getElementsByClassName("close")[0];
+
+  document.querySelectorAll(".img-contanier").forEach((item) => {
+    item.addEventListener("click", function () {
+      const img = this.querySelector("img");
+      modal.style.display = "block";
+      modalImg.src = img.src;
+    });
+  });
+
+  closeBtn.onclick = function () {
+    modal.style.display = "none";
+  };
+
+  window.onclick = function (event) {
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  };
 });
